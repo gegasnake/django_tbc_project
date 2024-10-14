b22adad1a0ee24fcbdb750e1e928ed5a65791e57
gegasnake <gegatvara@gmail.com>
2024-10-14 16:25:53 +0400

    updated models, added templates and listed categories with products

--- store/urls.py
+++ store/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from django_tbc_project import settings
-from .views import CategoryListView, ProductListView
+from .views import CategoryListView, ProductListView, ProductDetailView
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='category-list'),
-    path('products/', ProductListView.as_view(), name='product-list'),
+    path('categories/<int:category_id>/products/', ProductListView.as_view(), name='category-by-products'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),  # New URL for product detail
 ]
 
 
--- store/views.py
+++ store/views.py
@@ -1,36 +1,59 @@
-from django.http import JsonResponse
+from django.db.models import Avg, Sum
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Category, Product
 
 
 class CategoryListView(View):
     def get(self, request, *args, **kwargs):
-        categories = Category.objects.filter()
-        # I am getting all categories with its children
+        categories = Category.objects.filter(parent__isnull=True)
+
+        # I am getting all categories with its children and its parent
         category_data = [
             {
                 'id': category.id,
                 'name': category.name,
-                'children': [{'id': child.id, 'name': child.name} for child in category.children.all()]
+                'parent': [category.parent.id, category.parent.name] if category.parent is not None else None,
+                'children': [child.name for child in category.get_children()],
+                'product_count': category.get_product_count(),
             }
             for category in categories
         ]
-        return JsonResponse(category_data, safe=False)
+        return render(request, 'category.html', {'category_data': category_data})
 
 
 class ProductListView(View):
-    def get(self, request, *args, **kwargs):
-        products = Product.objects.all()
-        # getting all products with loop
-        # it does pretty same things like Category view but it shows us all products.
+    def get(self, request, category_id, *args, **kwargs):
+        category = get_object_or_404(Category, id=category_id)
+        subcategories = category.get_children()
+        products = Product.objects.filter(categories__in=[category] + list(subcategories))
         product_data = [
             {
                 'id': product.id,
                 'name': product.name,
-                'image': "http://127.0.0.1:8000" + product.image.url,
-                'categories': [{'id': category.parent.id, 'name': category.parent.name}
-                               for category in product.categories.all()]
+                'image': request.build_absolute_uri(product.image.url) if product.image and
+                                                                          product.image.name else None,
+                'sum': product.price * product.quantity,
             }
             for product in products
         ]
-        return JsonResponse(product_data, safe=False)
+        most_expensive_price = products.order_by('price').last().price
+        most_cheap_price = products.order_by('price').first().price
+        avg_price = products.aggregate(Avg('price'))['price__avg']
+        all_sum_price = sum([p.price * p.quantity for p in products])
+
+        return render(request, 'products_by_category.html',
+                      {'product_data': product_data,
+                               'max_price': most_expensive_price,
+                               'min_price': most_cheap_price,
+                               'avg_price': avg_price,
+                               'all_sum_price': all_sum_price,
+                       })
+
+
+class ProductDetailView(View):
+    def get(self, request, pk, *args, **kwargs):
+        product = get_object_or_404(Product, pk=pk)
+        total_value = product.quantity * product.price
+        return render(request, 'details_of_product.html', {'product': product,
+                                                           'total_value': total_value})
